88294b66f459490980866c37f94560fe82752887
Andreas Schneider <an.schneider@aon.at>
2016-01-22 18:44:33 +0100

    Mittendrin statt nur dabei .. beim Cashier natürlich

--- src/main/scala/fhj/swengb/pizza/PizzaDealerApp.scala
+++ src/main/scala/fhj/swengb/pizza/PizzaDealerApp.scala
@@ -21,88 +21,136 @@ object PizzaDealerApp {
 }
 
 
-case class CustomerAnim(obj:ImageView){
-  val customerImageList = List("/fhj/swengb/pizza/customer/customer1_angry.png","/fhj/swengb/pizza/customer/customer1_angry_glow.png","/fhj/swengb/pizza/customer/customer1_happy_glow.png","/fhj/swengb/pizza/customer/customer1_neutral.png","/fhj/swengb/pizza/customer/customer1_neutral_glow.png")
+case class CustomerAnim(){
+  var customerNr = 1
+  var obj:ImageView = _
+  var customerImageList = List("")
+  var customerAnim:ImageViewSprite = _
 
-  var dealerObj:ImageViewSprite = new ImageViewSprite(obj, new Image("/fhj/swengb/pizza/sprites/pizza-dealer.png"), 4, 2, 8, 280, 280, 10)
+  def set(obj:ImageView, customerNr:Int) {
+    this.obj = obj
+    this.customerNr = customerNr
+    this.customerImageList = List("/fhj/swengb/pizza/customer/customer"+customerNr+"_angry.png",
+                                  "/fhj/swengb/pizza/customer/customer"+customerNr+"_angry_glow.png",
+                                  "/fhj/swengb/pizza/customer/customer"+customerNr+"_happy_glow.png",
+                                  "/fhj/swengb/pizza/customer/customer"+customerNr+"_neutral.png",
+                                  "/fhj/swengb/pizza/customer/customer"+customerNr+"_neutral_glow.png")
+    this.customerAnim = new ImageViewSprite(obj, new Image(customerImageList(4)),1, 1, 1, 100, 200, 1)
 
-  def setDealer() {
-    dealerObj = new ImageViewSprite(obj, new Image("/fhj/swengb/pizza/sprites/pizza-dealer.png"), 4, 2, 8, 280, 280, 10)
+    val fadetransition: FadeTransition = new FadeTransition(Duration.millis(1000), obj)
+    fadetransition.setFromValue(0)
+    fadetransition.setToValue(1)
+    fadetransition.playFromStart()
   }
 
   def setAngry() {
-    dealerObj = new ImageViewSprite(obj, new Image(customerImageList(0)), 1, 1, 1, 200, 400, 1)
+    customerAnim = new ImageViewSprite(obj, new Image(customerImageList(0)), 1, 1, 1, 100, 200, 1)
   }
   def setHappy() {
-    dealerObj = new ImageViewSprite(obj, new Image(customerImageList(2)), 1, 1, 1, 200, 400, 1)
+    customerAnim = new ImageViewSprite(obj, new Image(customerImageList(2)), 1, 1, 1, 100, 200, 1)
   }
-
   def setNeutral() {
-    dealerObj = new ImageViewSprite(obj, new Image(customerImageList(3)), 1, 1, 1, 200, 400, 1)
+    customerAnim = new ImageViewSprite(obj, new Image(customerImageList(3)), 1, 1, 1, 100, 200, 1)
   }
-
-  def getAnimation():ImageViewSprite = {
-    dealerObj
+  def animate(now: Long) = {
+    customerAnim.handle(now)
   }
 }
 
 
-case class GameLoop(obj : ImageView, startPane:AnchorPane) extends AnimationTimer{
 
 
-  var customer = new CustomerAnim(obj)
-
-  var fadetransition: FadeTransition = new FadeTransition(Duration.millis(500),obj)
-
-  //var happyCostumer: ImageViewSprite = new ImageViewSprite(obj, new Image("/fhj/swengb/pizza/sprites/customer1_sprite.png"), 3, 2, 6, 200, 400, 10)
+object Cashier extends AnimationTimer{
+  val xMitte = 0
+  val yMitte = 0
 
 
-  //var waitingCostumer: ImageViewSprite = new ImageViewSprite(obj, new Image("/fhj/swengb/pizza/sprites/pizza-dealer.png"), 4, 2, 8, 280, 280, 10)
-  //var angryCostumer: ImageViewSprite = new ImageViewSprite(obj, new Image("/fhj/swengb/pizza/sprites/pizza-dealer.png"), 4, 2, 8, 280, 280, 10)
+  var GoingTo: String = ""
+
+  var dealer: ImageView = _
+  var dealerSpriteAnim:ImageViewSprite = _
+  var status = 0
+  var pathTrans: PathTransition = _
+
+  def set(obj: ImageView) = {
+    this.dealer = obj
+    this.dealerSpriteAnim = new ImageViewSprite(obj, new Image("/fhj/swengb/pizza/sprites/pizza-dealer.png"), 4, 2, 8, 280, 280, 10)
+    this.status = 0
+    pathTrans = new PathTransition()
+    pathTrans.setDuration(new Duration(200))
+    pathTrans.setNode(obj)
+    pathTrans.setAutoReverse(true)
+  }
+
+  def goTo(GoToName:String){
+
+
+/*
+  if (goingDown) {
+    path.getElements.add(new MoveTo(xMitte, yMitte))
+    path.getElements().add(new CubicCurveTo(xMitte + 50, yMitte, xMitte + 200, yMitte, xEnde+xMitte, yMitte))
+  } else {
+    path.getElements.add(new MoveTo(xEnde+xMitte, yMitte))
+    path.getElements().add(new CubicCurveTo(xMitte + 200, yMitte, xMitte + 50, yMitte, xMitte, yMitte))
+
+    pathTrans.playFromStart()
+    */
+  }
+
+
+
+  override def handle(now: Long): Unit = {
+    /*this.dealer.handle(now)
+    if (status != 3) {
+      if (status == 0) {
+        obj.setTranslateX(obj.getTranslateX - 5)
+        if (obj.getTranslateX < -150) status = 1
+      } else {
+        obj.setTranslateX(obj.getTranslateX + 5)
+        if (obj.getTranslateX > 150)  status = 0
+      }
+    }*/
+      Some
+  }
+
+}
+
+
+
+
+case class GameLoop(obj : ImageView) extends AnimationTimer{
+
+  var customer = new CustomerAnim()
+  customer.set(obj,2)
 
   var fps = 100
   var lastFrame = System.nanoTime
   var lastLogicFrame = 0
 
-  menuDealerAnim.set(obj)
 
-  customer
 
   override def handle(now: Long): Unit = {
     val frameJump: Int = Math.floor((now - lastFrame) / (1000000000 / fps)).toInt       //berechne ob genug Zeit vergangen ist um einen neuen Frame anzuzeigen
     if (frameJump > 1) {                                                                //neuen Frame berechnen
       lastFrame = now
       lastLogicFrame += 1
-      customer.getAnimation().handle(now)
-      menuDealerAnim.animate()
+      customer.animate(now)
+      //menuDealerLogoAnim.handle(now)
     }
 
 
     //Nur zum testen:
     if (lastLogicFrame == 50) {
-
-      fadetransition.setFromValue(0)
-      fadetransition.setToValue(1)
-      fadetransition.playFromStart()
       customer.setAngry()
 
     }else  if (lastLogicFrame == 100) {
-
-      fadetransition.setFromValue(0)
-      fadetransition.setToValue(1)
-      fadetransition.playFromStart()
       customer.setHappy()
 
     }else if (lastLogicFrame > 150){
-
-      fadetransition.setFromValue(0)
-      fadetransition.setToValue(1)
-      fadetransition.playFromStart()
       customer.setNeutral()
 
       lastLogicFrame=0
     }
-
   }
 }
 
@@ -110,16 +158,19 @@ case class GameLoop(obj : ImageView, startPane:AnchorPane) extends AnimationTime
 
 
 
-object menuDealerAnim {
+object menuDealerLogoAnim extends AnimationTimer{
+  var dealer: ImageViewSprite = _
   var status: Int = 0
   var obj:ImageView = new ImageView()
 
   def set(dealer: ImageView) = {
     obj = dealer
+    this.dealer = new ImageViewSprite(obj, new Image("/fhj/swengb/pizza/sprites/pizza-dealer.png"), 4, 2, 8, 280, 280, 10)
     status = 0
   }
 
-  def animate() = {
+  override def handle(now: Long): Unit = {
+    this.dealer.handle(now)
     if (status != 3) {
       if (status == 0) {
         obj.setTranslateX(obj.getTranslateX - 5)
@@ -130,13 +181,12 @@ object menuDealerAnim {
       }
     }
   }
+
 }
 
 
 
 class PizzaDealerApp extends javafx.application.Application {
-
-
   val Css = "/fhj/swengb/pizza/PizzaDealer.css"
   val Fxml = "/fhj/swengb/pizza/PizzaDealer.fxml"
 
@@ -156,9 +206,11 @@ class PizzaDealerApp extends javafx.application.Application {
     }
 }
 
+
+
 //controller contains the description of the functionality of the application
 class PizzaDealerAppController extends PizzaDealerApp {
-  //attributes are being initialized (everything with an ID)
+  //attributes are being initialized (everything with an ID
 
   //Main Pane
   @FXML var menu: AnchorPane = _
@@ -191,6 +243,7 @@ class PizzaDealerAppController extends PizzaDealerApp {
 
 
 
+
   //initialize function executes the commands at startup for the main scene
 
   //animation for Menue slide ins and outs
@@ -217,25 +270,18 @@ class PizzaDealerAppController extends PizzaDealerApp {
 
 
   def startHighscoresPane(): Boolean = {
-
     animMenuPanes(highscoresPane, false, 1280/2+5)
     animMenuPanes(highscoresMenu, true)
-
   true
   }
 
 
-
   def startSinglePlayer(playerName: String): Boolean = {
-
-
-    status.setText("Play against the bot:")
-
+    status.setText("Play a little game:")
     animMenuPanes(gameMenu, true)
     animMenuPanes(gamePane, false, 1280/2+5)
     true
   }
-
 
 
   def highscoresMenuBack(): Unit = animMenuPanes(highscoresMenu, true)
@@ -262,14 +308,18 @@ class PizzaDealerAppController extends PizzaDealerApp {
   }
 
 
-
-  lazy val testCircleAnim:GameLoop = new GameLoop(logoAnimationImageView, gameMenu)
+//Test um bei begin Animation auszuführen-> FAIL .. reality hits you hard bro!
+  //lazy val logoSet = menuDealerLogoAnim.set(logoAnimationImageView)
+  lazy val logoSet = new GameLoop(logoAnimationImageView)
+  //lazy val logoAnim = menuDealerLogoAnim
+  lazy val logoAnim = logoSet
 
   def exit(): Unit = {
+    logoSet
     progressBarTest.setProgress(progressBarTest.getProgress + 0.1)
     if (progressBarTest.getProgress > 1) System.exit(1)
-    else if (progressBarTest.getProgress % 0.2 < 0.1) testCircleAnim.stop()
-    else testCircleAnim.start();
+    else if (progressBarTest.getProgress % 0.2 < 0.1) logoAnim.stop()
+    else  logoAnim.start()
     print(progressBarTest.getProgress % 0.2 + "\n")
   }
 }
